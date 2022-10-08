9ab2d328a264b4f195bdeaff8a60e02982a9096e
Matheus Campos <theuscampos45@gmail.com>
2022-10-08 14:09:18 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "Tamanho máximo de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "Duração de 1 a 300 min")]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "Tamanho máximo de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "Duração de 1 a 300 min")]
+        public int Duracao { get; set; }
+    }
+}
