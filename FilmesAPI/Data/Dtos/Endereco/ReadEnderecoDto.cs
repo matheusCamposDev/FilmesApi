3c339ca19aa40f333628a22e971ffc6b4fb8b3f0
Matheus Campos <theuscampos45@gmail.com>
2022-12-20 15:00:25 -0300

    Adicionando Endereco

---
+++ FilmesAPI/Data/Dtos/Endereco/ReadEnderecoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmesAPI.Data.Dtos.Endereco
+{
+    public class ReadEnderecoDto
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+    }
+}
