2d9cb31b8fceb0d17594db2d52147831e8d2cd32
Matheus Campos <theuscampos45@gmail.com>
2022-12-20 14:20:43 -0300

    Adicionando Cinema

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -4,7 +4,6 @@ using FilmesAPI.Data;
 using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,10 +13,10 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private FilmeContext _context;
+        private AppDbContext _context;
         private IMapper _mapper;
 
-        public FilmeController(FilmeContext context, IMapper mapper)
+        public FilmeController(AppDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
---
+++ FilmesAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+    }
+}
